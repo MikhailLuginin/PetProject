b70947fffaa0412e7c139b1b3bdc6458ebdbbae0
Mikhail Luginin <m.luginin@gk-adept.ru>
2025-10-02 20:44:59 +0300

    add

--- conftest.py
+++ conftest.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 pytest_plugins = (
@@ -8,9 +10,18 @@ pytest_plugins = (
 @pytest.fixture(scope="session")
 def browser_context_args(browser_context_args):
     return {
-
+        **browser_context_args,
         "viewport": {
             "width": 1920,
             "height": 1080,
         }
     }
+
+
+@pytest.fixture(scope="session")
+def launch_options():
+    """Настройки запуска браузера (headless / headed)"""
+    if os.getenv("CI") == "true":
+        return {
+            "headless": True,
+        }
