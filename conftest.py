5c2c79db860865fefb013e5a9d1ccbfa0f7cb447
Mikhail Luginin <m.luginin@gk-adept.ru>
2025-10-06 22:43:54 +0300

    add

--- conftest.py
+++ conftest.py
@@ -9,8 +9,9 @@ load_dotenv()
 
 
 @pytest.fixture(scope="session")
-def browser_context_args():
+def browser_context_args(browser_context_args):
     return {
+        **browser_context_args,
         "viewport": {
             "width": 1920,
             "height": 1080,
